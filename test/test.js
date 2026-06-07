911a83aad367b28af24d0b358bd20949cba0b830
Evan Hahn <me@evanhahn.com>
2026-06-07 12:05:20 +0000

    Use Node's built-in test runner

--- test/test.js
+++ test/test.js
@@ -1,14 +1,13 @@
-var getLatestNodeVersion = require('..');
+const getLatestNodeVersion = require('..');
 
-var semver = require('semver');
-var assert = require('assert');
+const semver = require('semver');
+const assert = require('node:assert/strict');
+const test = require('node:test');
 
-describe('getLatestNodeVersion', function () {
-  it('returns a semver-compatible string', function (done) {
-    getLatestNodeVersion(function (err, version) {
-      assert.equal(err, null);
-      assert.notEqual(semver.valid(version), null);
-      done();
-    });
+test('returns a semver-compatible string', function (_t, done) {
+  getLatestNodeVersion(function (err, version) {
+    assert.equal(err, null);
+    assert.notEqual(semver.valid(version), null);
+    done();
   });
 });
